ab32c586bc93ab25e56ce3cdedc6744da782b7e0
russel-07 <russel170790@gmail.com>
2023-06-10 00:50:37 +0300

    app_users

--- foodgram_project/app_users/urls.py
+++ foodgram_project/app_users/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
--- foodgram_project/foodgram_project/urls.py
+++ foodgram_project/foodgram_project/urls.py
@@ -21,9 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls')),
     path('auth/', include('app_users.urls')),
-    
 ]
 
 
