ffceef252906c0a9081be96572f2a0514c7b4ca1
russel-07 <russel170790@gmail.com>
2023-06-13 15:29:46 +0300

    app_users add ModelAdmin

--- foodgram_project/app_users/admin.py
+++ foodgram_project/app_users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+class UseAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('email', 'username')
+    empty_value_display = '-пусто-'
+
+
+admin.site.unregister(User)
+admin.site.register(User, UseAdmin)
--- foodgram_project/app_users/models.py
+++ foodgram_project/app_users/models.py
@@ -2,3 +2,5 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+User._meta.get_field('email').blank = False
