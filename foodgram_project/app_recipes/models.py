cef9870c03f66803eff50545bc56234c600c9133
russel-07 <russel170790@gmail.com>
2023-06-14 15:47:57 +0300

    app_recipes

--- foodgram_project/app_recipes/models.py
+++ foodgram_project/app_recipes/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class Unit(models.Model):
@@ -18,7 +22,7 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=50, unique=True,
                             verbose_name='название')
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE,
-                             related_name="units",
+                             related_name="ingredients",
                              verbose_name='единицы измерения')
 
     def __str__(self):
@@ -28,3 +32,52 @@ class Ingredient(models.Model):
         verbose_name = 'ингредиент'
         verbose_name_plural = 'ингредиенты'
         ordering = ['name']
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True,
+                            verbose_name='тег')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
+        ordering = ['name']
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='recipes')
+    name = models.CharField(max_length=50, unique=True,
+                            verbose_name='название')
+    image = models.ImageField(upload_to='recipes/', blank=True, null=True)
+    description = models.TextField(verbose_name='описание')
+    ingredients = models.ManyToManyField(Ingredient, through='Recipe'
+                                         'Ingredient', related_name='recipes')
+    tag = models.ManyToManyField(Tag, related_name='recipes')
+    cook_time = models.PositiveSmallIntegerField(verbose_name='время '
+                                                 'приготовления')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'рецепт'
+        verbose_name_plural = 'рецепты'
+        ordering = ['-pub_date']
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                               related_name='recipe_ingredients',
+                               verbose_name='рецепт')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE,
+                                   related_name='recipe_ingredients',
+                                   verbose_name='ингредиент')
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ("recipe", "ingredient")
