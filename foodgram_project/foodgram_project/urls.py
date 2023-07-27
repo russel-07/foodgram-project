ec684655d040979c07927207d68866d76b0b7005
russel-07 <russel170790@gmail.com>
2023-07-27 11:53:56 +0300

    pep8

--- foodgram_project/foodgram_project/urls.py
+++ foodgram_project/foodgram_project/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import handler404, handler500 
-from django.contrib.flatpages import views
 
 
 urlpatterns = [
@@ -20,5 +19,7 @@ handler500 = "app_recipes.misc.server_error"  # noqa
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
