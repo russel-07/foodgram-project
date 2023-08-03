7c0f63d4a57512fa73f56ab84036a85d4751982c
russel-07 <russel170790@gmail.com>
2023-08-03 16:26:45 +0300

    debugging

--- foodgram_project/foodgram_project/settings.py
+++ foodgram_project/foodgram_project/settings.py
@@ -30,8 +30,10 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '51.250.108.20',
     'web',
+    '51.250.108.20',
+    'russel.fun',
+    'www.russel.fun'
     ]
 
 CSRF_TRUSTED_ORIGINS = [
